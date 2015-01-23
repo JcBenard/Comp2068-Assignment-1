0efe191772337a69af6836255d03093a953331f5
JcBenard <niteburn@hotmail.com>
2015-01-23 14:40:32 -0500

    Added subweapon checking

--- comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
+++ comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
@@ -10,3 +10,11 @@ function subweapon(sub) {
     this.sub = sub;
     console.log(sub);
 }
+
+function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
+    if (sub == subNeeded) {
+        document.getElementById(bossName).href = "#/" + goodOutcome;
+    } else {
+        document.getElementById(bossName).href = "#/" + badOutcome;
+    }
+}

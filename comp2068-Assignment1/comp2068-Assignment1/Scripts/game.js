414fc83c23fcdb31cc94ce4ad5ea6d755d89593f
JcBenard <niteburn@hotmail.com>
2015-01-29 18:45:01 +0900

    Wrote out pages

--- comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
+++ comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
@@ -8,7 +8,7 @@ function init() {
 var sub;
 function subweapon(sub) {
     this.sub = sub;
-    console.log(sub);
+    document.getElementsByClassName(slide).style.backgroundImage = "url('../assets/images/sub.png')";
 }
 
 function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
@@ -19,6 +19,8 @@ function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
     }
 }
 
-function changeBack(id) {
-    document.getElementById(id).style.backgroundImage = "url('../assets/images/" + id + ".png')";
+function changeBack() {
+    for(i = 1; i <=38; i++){
+        document.getElementById(i).style.backgroundImage = "url('../assets/images/" + i + ".png')";
+    }
 }

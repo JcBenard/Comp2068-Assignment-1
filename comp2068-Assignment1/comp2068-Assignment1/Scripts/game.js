bb20ff24e169dc3f99c936e8c284926bb264823b
JcBenard <niteburn@hotmail.com>
2015-01-30 07:47:58 +0900

    Maked it so subweapon images show up and change

--- comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
+++ comp2068-Assignment1/comp2068-Assignment1/Scripts/game.js
@@ -1,26 +1,32 @@
 ﻿/// <reference path="lib/impress.js" />
+var sub;
 
 function init() {
     console.log("Game Started");
     impress().init();
+
+    //set up the background images for the slides
+    for (i = 1; i <= 38; i++) {
+        document.getElementById(i).style.backgroundImage = "url('../assets/images/" + i + ".png')";
+    }
 }
 
-var sub;
+//change the subwepon when they get a new one
 function subweapon(sub) {
     this.sub = sub;
-    document.getElementsByClassName(slide).style.backgroundImage = "url('../assets/images/sub.png')";
+
+    var subImgs = document.getElementsByTagName("img")
+
+    for (x = 0; x <= subImgs.length; x++) {
+        subImgs[x].src = "/assets/images/" + this.sub + ".png"
+    }
 }
 
+//see if the player has a required subweapon
 function subCheck(subNeeded, goodOutcome,badOutcome, bossName) {
     if (sub == subNeeded) {
         document.getElementById(bossName).href = "#/" + goodOutcome;
     } else {
         document.getElementById(bossName).href = "#/" + badOutcome;
-    }
-}
-
-function changeBack() {
-    for(i = 1; i <=38; i++){
-        document.getElementById(i).style.backgroundImage = "url('../assets/images/" + i + ".png')";
     }
 }
